ab0cc2f176630f49ad8737134af51f01952ced6b
Dorine <dodoxgugus@gmail.com>
2022-05-07 08:59:39 +0200

    fix: redirect and UTF8 descripttion

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import viewsRoutes from 'views/routes';
 
 const Routes = () => {
   return (
-    <BrowserRouter basename='/'>
-      <Switch>
-        {viewsRoutes.map((item, i) => (
-          <Route key={i} exact path={item.path} render={() => item.renderer()} />
-        ))}
-        <Redirect status={404} to={'/not-found-cover'} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      {viewsRoutes.map((item, i) => (
+        <Route key={i} exact path={item.path} render={() => item.renderer()} />
+      ))}
+      <Redirect status={404} to={'/not-found-cover'} />
+    </Switch>
   );
 };
 
